bf435112e1727d362299a2c7958ab641e5f64140
fioretxv <143533169+fioretxv@users.noreply.github.com>
2023-12-17 01:56:07 -0500

    Created Win Conditions and Force Reset

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ let timesplayed =0;
 let rocksclimb =0;
 function run(x) {
     let rocksclimb = sessionStorage.getItem("won");
+    if (rocksclimb >= 10) {
+        alert("You Made it Out! Reset Score to Try Again!")
+    } else {
     let timesplayed = sessionStorage.getItem("gameCount");
     let choiceresult = x;
     let randNum = Math.floor(Math.random() * 10);
     let chanceresult;
+    let percent;
     let gameresult;
     if (randNum === 1) {
         chanceresult = 'Rock 1';
@@ -14,11 +18,40 @@ function run(x) {
     };
     if (chanceresult === choiceresult){
         gameresult = "You climbed higher!";
-        rocksclimb+++++;
+        rocksclimb++;
     } else {
         gameresult = "That Rock is Too Hot!";
     };
     timesplayed++;
+    if (rocksclimb === 1) {
+        percent = 'You are 10% to the Top!';
+    } else if (rocksclimb === 2) {
+        percent = 'You are 20% to the top!';
+    }else if (rocksclimb === 3) {
+        percent = 'You are 30% to the top!';
+    }else if (rocksclimb === 4) {
+        percent = 'You are 40% to the top!';
+    }else if (rocksclimb === 5) {
+        percent = 'You are half way to the top!';
+    }else if (rocksclimb === 6) {
+        percent = 'You are 60% to the top!';
+    }else if (rocksclimb === 7) {
+        percent = 'You are 70% to the top!';
+    }else if (rocksclimb === 8) {
+        percent = 'You are 80% to the top!';
+    }else if (rocksclimb === 9) {
+        percent = 'You are 90% to the top!';
+    }else if (rocksclimb === 10) {
+        percent = 'You Climbed Out of the Volcano!';
+    }else {
+        percent = 'You Climbed no Further!';
+    };
+   if (rocksclimb === 10){
+        alert("You Made it Out! Reset Score to Try Again!")
+    };
+
+    let percentclimb = document.getElementById('percent');
+    percentclimb.innerText = percent;
 
     let chance = document.getElementById('chance');
     chance.innerText = chanceresult;
@@ -33,7 +66,7 @@ function run(x) {
     let won = document.getElementById('wins');
     won.innerText = rocksclimb;
     sessionStorage.setItem("won",rocksclimb)
-}
+} }
 function reset(){
     sessionStorage.setItem("won",0);
     sessionStorage.setItem("gameCount",0);
